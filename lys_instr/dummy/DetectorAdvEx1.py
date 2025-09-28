5816d54b1be18ba59840fd3b4252fcbf15bcf42c
zwang154 <zwang154@alumni.jh.edu>
2025-09-28 10:13:43 +0900

    Examples updated.

--- lys_instr/dummy/DetectorAdvEx1.py
+++ lys_instr/dummy/DetectorAdvEx1.py
@@ -18,18 +18,20 @@ class DetectorAdvEx1Dummy(MultiDetectorDummy):
         allIndices = list(itertools.product(*(range(s) for s in data.shape[:3])))
         totalFrames = len(allIndices)
 
-        for _ in range(iter):
-            if self._count >= totalFrames:
-                self._count = 0
-            if self._shouldStop:
-                return
-            time.sleep(self.exposure)
-            idx = allIndices[self._count]
-            self._frame = data[idx]
-            self._data[idx] = data[idx][1]
-            self._axes = [np.linspace(0, 1, s) for s in self.indexShape[:2]] + [np.linspace(0, 360, data.shape[2], endpoint=False), data[idx][0]]
-            self.updated.emit()
-            self._count += 1
+        i = 0
+        while i != iter:
+            for idx in itertools.product(*[range(j) for j in self.indexShape]):
+                if self._count >= totalFrames:
+                    self._count = 0
+                if self._shouldStop:
+                    return
+                time.sleep(self.exposure)
+                idx = allIndices[self._count]
+                self._data[idx[-1] if self.indexShape[-1] > 1 else 0] = data[idx][1]
+                self._axes = [np.linspace(0, 360, data.shape[2], endpoint=False), data[idx][0]] if self.indexShape[-1] > 1 else data[idx][0]
+                self.updated.emit()
+                self._count += 1
+            i += 1
 
     @property
     def axes(self):
--- lys_instr/tutorial/AdvEx2_1.py
+++ lys_instr/tutorial/AdvEx2_1.py
@@ -2,36 +2,19 @@ from lys.widgets import LysSubWindow
 from lys.Qt import QtWidgets
 from lys_instr import DataStorage, PreCorrector, gui, dummy
 from lys_instr.gui.MultiScan import ScanWidget
-from lys import multicut, Wave
-import numpy as np
-import pyqtgraph as pg
-
-
-# class DetectorAdvEx2GUI(gui.MultiDetectorGUI):
-    # def createDisplayWidget(self):
-    #     self._mcut = multicut(Wave(np.random.rand(*self._obj.dataShape), *self._obj.axes), returnInstance=True, subWindow=False)
-    #     self._mcut.widget.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-
-
-
-class DetectorAdvEx2GUI(gui.MultiDetectorGUI):
-    def _update(self):
-        i, j = divmod(self._frameCount - 1, self._obj.indexShape[0])
-        self._indexView.setImage(np.mean(self._data.data, axis=(2, 3)))
-        self._frameView.setImage(self._data[j].data)
 
 
 class AppWindow(LysSubWindow):
     def __init__(self, parent=None):
         super().__init__(parent)
-        self.setWindowTitle("Advanced Example #2-2")
+        self.setWindowTitle("Advanced Example #2-1")
         self._storage = DataStorage()
         self._detector = dummy.MultiDetectorDummy((8, 8), (256, 256), exposure=0.1)
         self._motor = dummy.MultiMotorDummy("x", "y", "t")
         self._pre = PreCorrector([self._motor])
         self._switch = dummy.MultiSwitchDummy("A")
         self._storage.connect(self._detector)
-        self.setSettingFile("AdvEx2.dic")
+        self.setSettingFile("AdvEx2_1.dic")
         self._initLayout()
         
         self.adjustSize()
@@ -40,7 +23,7 @@ class AppWindow(LysSubWindow):
         _storageGUI = gui.DataStorageGUI(self._storage)
         _motorGUI = gui.MultiMotorGUI(self._motor, axisNamesSettable=("x", "y", "t"), axisNamesJoggable=("t"), axisNamesOffsettable=("x", "y", "t"))
         _switchGUI = gui.MultiSwitchGUI(self._switch)
-        _detectorGUI = DetectorAdvEx2GUI(self._detector)
+        _detectorGUI = gui.MultiDetectorGUI(self._detector)
         _correctorGUI = gui.PreCorrectorGUI(self._pre)
         _scanGUI = ScanWidget(self._storage, [self._switch, self._motor], {"MultiDetectorDummy": self._detector}, numScans=2)
 
@@ -64,33 +47,25 @@ class AppWindow(LysSubWindow):
 
         # Set multicut display style
         mcut = _detectorGUI._mcut
-
-        self._indexView = pg.ImageView()
-        self._indexView.ui.roiBtn.hide()
-        self._indexView.ui.menuBtn.hide()
-        self._indexView.ui.histogram.hide()
-        self._indexView.view.setAspectLocked(False)
-        self._indexView.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        mcut._grid.layout.addWidget(self._indexView, 0, 0, 4, 2)
-        _detectorGUI._indexView = self._indexView
-
-        # Add live frame display
-        self._frameView = pg.ImageView()
-        self._frameView.ui.roiBtn.hide()
-        self._frameView.ui.menuBtn.hide()
-        self._frameView.ui.histogram.hide()
-        self._frameView.view.setAspectLocked(False)
-        self._frameView.setSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
-        mcut._grid.layout.addWidget(self._frameView, 0, 2, 4, 2)
-        _detectorGUI._frameView = self._frameView
+        wave1 = mcut.cui._children.addWave([0, 1])
+        canvas1 = mcut.display(wave1, type="grid", pos=(0, 0), wid=(4, 2))
+        wave2 = mcut.cui._children.addWave([2, 3])
+        canvas2 = mcut.display(wave2, type="grid", pos=(0, 2), wid=(4, 2))
+        mcut._can.addRect(canvas1)
+        rect = canvas1.getRectAnnotations()[0]
+        rect.setRegion([[0.4, 0.6], [0.4, 0.6]])
+        canvas1.update()
 
         # Set scan parameters
+        # Set scan axis 0 (first row) to 'A', free mode, value [True]
         _scanGUI._scanRangeRows[0]._scanAxis.setCurrentText("A")
         _scanGUI._scanRangeRows[0]._scanMode.setCurrentText("Free")
         _scanGUI._scanRangeRows[0]._freeExpr.setText(str([True,]))
+        # Set scan axis 1 (second row) to 't', linear mode, from 0, step 0.1, 9 steps
         _scanGUI._scanRangeRows[1]._scanAxis.setCurrentText("t")
         _scanGUI._scanRangeRows[1]._step.setValue(0.1)
         _scanGUI._scanRangeRows[1]._numSteps.setValue(9)
+        # Set reference axis to 'A', reference value to False (pump OFF)
         _scanGUI._check_ref.setChecked(True)
-        _scanGUI._combo_ref.setCurrentText("A")      # Reference axis is "A"
-        _scanGUI._value_ref.setValue(False)          # Reference value is pump OFF
+        _scanGUI._combo_ref.setCurrentText("A")
+        _scanGUI._value_ref.setValue(False)
