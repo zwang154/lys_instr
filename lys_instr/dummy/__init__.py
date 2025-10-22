6a109c55b795dda06d9a1aa43dc4994f964fc4d6
zwang154 <zwang154@alumni.jh.edu>
2025-10-22 21:18:54 +0900

    Clean up unnecessary files.

--- lys_instr/dummy/__init__.py
+++ lys_instr/dummy/__init__.py
@@ -1,5 +1,3 @@
 from .MultiDetector import MultiDetectorDummy
 from .MultiMotor import MultiMotorDummy
 from .MultiSwitch import MultiSwitchDummy
-from .DetectorEx1 import DetectorEx1Dummy
-from .DetectorEx2 import DetectorEx2Dummy
