1a63e7900d21d28a3e33e295b80ac4a2fe6c0174
zwang154 <zwang154@alumni.jh.edu>
2025-11-01 18:11:02 +0900

    Updated requirements and setup.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="lys_instr",
     packages=find_packages(exclude=("test*",)),
     version="0.1.0",
-    description="A Python package for instrument control and automated measurements.",
+    description="A Python package for automating scientific measurements.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     author="Ziqian Wang",
